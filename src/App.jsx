acdcd7e7f326d8bac3acc403db3027613b1a20b0
melina-escalante <meliescalantee@gmail.com>
2025-07-17 21:25:04 -0300

    style: mejorar estructura del main, componentizar navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import { HomeSection } from './sections/HomeSection'
-import { Navbar } from './components/Navbar'
-import { TechnologiesSection } from './sections/TechnologiesSection'
-import { ProjectSection } from './sections/ProjectSection'
-import { Footer } from './components/Footer'
-import { ContactSection } from './sections/ContactSection'
+import { useState } from "react";
+import { HomeSection } from "./sections/HomeSection";
+import { Navbar } from "./components/Navbar";
+import { TechnologiesSection } from "./sections/TechnologiesSection";
+import { ProjectSection } from "./sections/ProjectSection";
+import { Footer } from "./components/Footer";
+import { ContactSection } from "./sections/ContactSection";
 function App() {
-
   return (
     <>
-      <main className=' pb-10 mx-auto'>
-        <Navbar></Navbar>
+      <Navbar></Navbar>
+      <main className="  mx-auto">
         <HomeSection></HomeSection>
         <TechnologiesSection></TechnologiesSection>
         <ProjectSection></ProjectSection>
-        <ContactSection>
+      </main> <div className="pb-14">
 
+      <ContactSection>
         <Footer></Footer>
-        </ContactSection>
-      </main>
+      </ContactSection>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
