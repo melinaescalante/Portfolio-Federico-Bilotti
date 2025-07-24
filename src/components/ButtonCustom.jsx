8aff6855b780e860867b316b73ed9b536804c56b
melina-escalante <meliescalantee@gmail.com>
2025-07-24 19:16:59 -0300

    style: cambiar button custom a A

--- src/components/ButtonCustom.jsx
+++ src/components/ButtonCustom.jsx
@@ -1,14 +1,14 @@
 export function ButtonCustom({ text, textStyles, containerStyles, icon, url, t='_self' }) {
     return (
         <>
-            <button className={`z-10 rounded-lg  cursor-pointer px-8 py-3 justify-center flex gap-3 ${containerStyles}`}>
-                <a target={t} href={url} download={url!=='#projects'}>
+            <a target={t} href={url} download={url!=='#projects'} className={`z-10 rounded-lg  cursor-pointer px-8 py-3 justify-center flex gap-3 ${containerStyles}`}>
+           
                     <div className={`${textStyles} flex gap-3`}>
                         {text}
                         <span dangerouslySetInnerHTML={{ __html: icon }}></span>
                     </div>
-                </a>
-            </button>
+                
+            </a>
         </>
     )
 }
